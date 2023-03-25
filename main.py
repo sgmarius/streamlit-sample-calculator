8efe78edfb3cac0d5a32456ba34409d3cb314016
mariussandu <marius@ltng-bi.com>
2023-03-25 12:01:17 +0200

    Updated to tabs

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-from apps.SampleSizeCalculator.Home import home_page
+import streamlit as st
+from apps.SampleSizeCalculator.home import home_page
 from apps.SampleSizeCalculator.pages.calculator import calculator_page
 
-calculator_page()
+
+home, calculator = st.tabs(["Home", "Calculator"])
+
+with home:
+   st.header("Home page")
+   home_page()
+
+with calculator:
+   st.header("Calculator")
+   calculator_page()
